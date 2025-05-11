93ab83f66540e7b2dd3e964f656ad9162d00dd3b
st10275468 <st10275468@vcconnect.edu.za>
2025-05-11 16:09:43 +0200

    Populated Product class with attributes and fields

--- Agri-Energy Connect Platform/Models/Product.cs
+++ Agri-Energy Connect Platform/Models/Product.cs
@@ -1,6 +1,25 @@
-﻿namespace Agri_Energy_Connect_Platform.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Agri_Energy_Connect_Platform.Models
 {
     public class Product
     {
+        [Key]
+        public int productID { get; set; }
+
+        [Required]
+        public string productName { get; set; }
+
+        [Required]
+        public string category {  get; set; }
+
+        public string description { get; set; }
+
+        [ForeignKey("Farmer")]
+        public int userID {  get; set; }
+
+        public User Farmer { get; set; }
+
     }
 }
