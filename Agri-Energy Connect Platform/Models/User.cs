306ab28893517ce6ec4a73f80d9392af10dce4f8
st10275468 <st10275468@vcconnect.edu.za>
2025-05-11 16:04:49 +0200

    Populated the User class with attributes and fields

--- Agri-Energy Connect Platform/Models/User.cs
+++ Agri-Energy Connect Platform/Models/User.cs
@@ -1,6 +1,30 @@
-﻿namespace Agri_Energy_Connect_Platform.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Agri_Energy_Connect_Platform.Models
 {
     public class User
     {
+        [Key]
+        public int userID {  get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public string surname { get; set; }
+        
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        [Required]
+        public string phoneNumber { get; set; }
+
+        [Required]
+        public string role {  get; set; }
+
+        public ICollection<Product> farmerProducts { get; set; }
     }
 }
